034a8328bc304b0f4e744a935d8211a5983204d2
CeCamillo <cesarcamillo2212@gmail.com>
2024-04-20 13:21:57 -0300

    feat: scaffold UI

--- recipes-front/README.md
+++ recipes-front/README.md
@@ -1,10 +1,11 @@
 '''
+
 # Recipes Frontend
 
 ## TODO
 
-- [ ] Make it deploy
-- [ ] Scaffold basic UI with mock data
+- [x] Make it deploy
+- [x] Scaffold basic UI with mock data
 - [ ] Attach database to UI
 - [ ] Add auth
 - [ ] Add recipe creation
--- recipes-front/src/app/page.tsx
+++ recipes-front/src/app/page.tsx
@@ -1,9 +1,49 @@
 import Link from "next/link";
 
+const mockData = [
+  {
+    id: 1,
+    title: "Receita Pão de Queijo",
+    name: "Pão de Queijo",
+    ingredients: "Farinha, Trigo e Queijo",
+    preparation: "Preparation for Pão de Queijo",
+  },
+  {
+    id: 2,
+    title: "Receita Bolo de Chocolate",
+    name: "Bolo de Chocolate",
+    ingredients: "Farinha, Trigo,Ovo, Manteiga, Chocolate",
+    preparation: "Preparation for Bolo de Chocolate",
+  },
+  {
+    id: 3,
+    title: "Receita Omelete",
+    name: "Omelete",
+    ingredients: "Ovo, Presunto, Queijo, Tomate, Cebola",
+    preparation: "Preparation Omelete",
+  },
+];
+
+// const mockDataItem = mockData.map((item) => ({
+//   ...item,
+// }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Frontend in progress...
+    <main className="">
+      <div className="flex flex-wrap justify-center gap-4">
+        {[...mockData, ...mockData, ...mockData].map((item) => (
+          <div key={item.id} className="w-48 border">
+            <h2>{item.title}</h2>
+            <p>{item.name}</p>
+            <p>{item.ingredients}</p>
+            <p>{item.preparation}</p>
+            {/* <Link href={`/recipe/${item.id}`}>
+              <a>Ver receita</a>
+            </Link> */}
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
