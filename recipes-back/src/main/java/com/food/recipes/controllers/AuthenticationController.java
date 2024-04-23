6d586991f4d7d69ad1c58d350f17e6750eceb8f4
Igor Nascimento <igor.nacimento51@gmail.com>
2024-04-23 12:20:21 -0300

    feat: add documentation for authentication

--- recipes-back/src/main/java/com/food/recipes/controllers/AuthenticationController.java
+++ recipes-back/src/main/java/com/food/recipes/controllers/AuthenticationController.java
@@ -5,6 +5,11 @@ import com.food.recipes.model.dto.security.AuthenticationRequest;
 import com.food.recipes.model.dto.security.AuthenticationResponse;
 import com.food.recipes.model.dto.security.RegisterRequest;
 import com.food.recipes.services.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
+@Tag(name = "Authentication", description = "Endpoint for managing authentication")
 @RequiredArgsConstructor
 @Slf4j
 @RestController
@@ -29,13 +35,23 @@ public class AuthenticationController {
     private final AuthenticationService service;
 
     @PostMapping("/register")
+    @Operation(summary = "Adds a new User!",
+            description = "Adds a new User by passing his data!",
+            tags = {"Authentication"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = AuthenticationResponse.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<?> register(
             @RequestBody @Valid RegisterRequest request,
             HttpServletRequest httpServletRequest
-            ) {
+    ) {
         try {
             var register = service.register(request);
-            return ResponseEntity.ok(new AuthenticationResponse(register.getName(), register.getToken()));
+            return ResponseEntity.status(HttpStatus.CREATED).body(new AuthenticationResponse(register.getName(), register.getToken()));
         } catch (DataIntegrityViolationException ex) {
             log.info("The username: {} already exists", request.getEmail());
             return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorMessage
@@ -51,6 +67,16 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Authenticates an User!",
+            description = "Authenticates an User!",
+            tags = {"Authentication"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = AuthenticationResponse.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<?> login(
             @RequestBody @Valid AuthenticationRequest request,
             HttpServletRequest httpServletRequest
